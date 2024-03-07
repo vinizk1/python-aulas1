a80d92050202b069543eb6eab4cb4c9e2a89219d
vinizk1 <128698338+vinizk1@users.noreply.github.com>
2024-03-07 12:11:16 -0300

    Add files via upload

--- medianotas.py
+++ medianotas.py
@@ -1,8 +1,15 @@
-nota1 = int(input("digite a nota 1:"))
-nota2 = int(input("digite a nota 2:"))
-nota3 = int(input("digite a nota 3:"))
+#nota1 = int(input("digite a nota 1:"))
+#nota2 = int(input("digite a nota 2:"))
+#nota3 = int(input("digite a nota 3:"))
 
-somaTotal = (nota1 + nota2 + nota3)
-media = somaTotal / 3
+#somaTotal = (nota1 + nota2 + nota3)
+#media = somaTotal / 3
 
-print(f"sua média é {media}")
+#print(f"sua média é {media}")
+
+n = int(input("Digite um numero"))
+
+if n % 2 == 0:
+    print("seu numero é par")
+else:
+    print("seu numero é impar")
