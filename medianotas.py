bee645f8c06849f165cea7501fb268cc82123248
vinizk1 <128698338+vinizk1@users.noreply.github.com>
2024-03-07 12:06:51 -0300

    Add files via upload

---
+++ medianotas.py
@@ -0,0 +1,8 @@
+nota1 = int(input("digite a nota 1:"))
+nota2 = int(input("digite a nota 2:"))
+nota3 = int(input("digite a nota 3:"))
+
+somaTotal = (nota1 + nota2 + nota3)
+media = somaTotal / 3
+
+print(f"sua média é {media}")
